0e1a94259c3c8a3ab158cb81492d6ee9d884bd6a
keenan137 <keenan.dup137@gmail.com>
2023-12-09 00:59:58 +0200

    Looking at some of the basic types and what it means.

--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
+// Explicitly setting the types
 let age: number =  30;
-console.log(age);
+let firstName: string = "Mario";
+let isFictional: boolean;
+
+// Inference
+let planet = 'Earth';
+let moons = 1;
+let isLarge = false;
+
+// null & undefined properties
+let something: null;
+let anotherThing: undefined;
