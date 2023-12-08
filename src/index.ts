7f46a8a9792f09291daf7ecbc7dbf36def689d0e
keenan137 <keenan.dup137@gmail.com>
2023-12-09 01:27:43 +0200

    Went through examples of all the basic types.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,61 @@
-// Explicitly setting the types
+// Examples of explicitly setting some types
 let age: number =  30;
 let firstName: string = "Mario";
 let isFictional: boolean;
-
-// Inference
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+// Type Inference - TypeScript will guess intelligently for us.
 let planet = 'Earth';
 let moons = 1;
 let isLarge = false;
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+// null & undefined properties (not very useful but is possible)
+let something: null = null;
+let anotherThing: undefined = undefined;
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+// Arrays (also has type inference)
+let names: string[] = ["apples"];
+// names.push(39);
+names.push("test");
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+// Object Literals (also has type inference)
+let user: {
+    name: string,
+    age: number,
+    id: number,
+} = {
+    name: 'Mario',
+    age: 30,
+    id: 1
+}
+//user.email = "luigi@gmail.com";
+user.name = "Luigi";
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+// Functions (also has type inference)
+function addTwoNumbers(a: number, b: number){
+    console.log(a+b)
+    return a+b;
+}
+// addTwoNumbers(1, "2");
+addTwoNumbers(1, 2);
 
-// null & undefined properties
-let something: null;
-let anotherThing: undefined;
+const subtractTwoNumbers = (a: number, b: number): number =>{
+    console.log(a-b);
+    return a-b;
+}
+// subtractTwoNumbers(5, true);
+subtractTwoNumbers(5, 4);
+
+function addAllNumbers(items: number[]): number{
+    const total = items.reduce((a,b) => a+b, 0);
+    console.log(total);
+    return total;
+};
+// addAllNumbers([1,2,3,true]);
+addAllNumbers([1,2,3,4]);
+//----------------------------------------------------------------
+//----------------------------------------------------------------
